e8aac50286a536d2f9a071072caea8d59fd23c52
bdjekel <bdjekel@gmail.com>
2024-08-26 17:18:18 -0500

    small update

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def main():
         print("screen working")
 
     while True:
-        screen.fill(color=(0,0,0))
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill((0,0,0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
