ed844437fd761e3b4cdd924eff0581ab2d906377
bdjekel <bdjekel@gmail.com>
2024-08-28 11:48:09 -0500

    added update and rotate methods to Player class, updated main.py to update Player instance every loop, added player rotation speed variable

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    dt = game_clock.tick(60) / 1000
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill((0,0,0))
         player.draw(screen)
-        dt = game_clock.tick(60) / 1000
-
-
         pygame.display.flip()
 
 if __name__ == "__main__":
