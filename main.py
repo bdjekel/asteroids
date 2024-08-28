c232c876270145cf84220533bd2c0085b9e10c0a
bdjekel <bdjekel@gmail.com>
2024-08-28 11:20:31 -0500

    added Player class module, updated main to create instance of Player

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -13,8 +15,10 @@ def main():
                 return
 
         screen.fill((0,0,0))
+        player.draw(screen)
         dt = game_clock.tick(60) / 1000
-        print(dt)
+
+
         pygame.display.flip()
 
 if __name__ == "__main__":
